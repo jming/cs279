e26a82ca9f14ae6282b16975559800b232553311
jming <jming@college.harvard.edu>
2014-09-10 22:30:50 -0400

    able to click through and highlighed targets

--- ribbon.js
+++ ribbon.js
@@ -1,8 +1,11 @@
+// global variables
+
 var canvas = document.getElementById('ribbon-canvas');
 var context = canvas.getContext('2d');
 var current_page = 0;
+var target_rect = 11;
 
-// initialize the basic initial screen
+// initialize the basic screen
 
 var head = new Image();
 var home = new Image();
@@ -14,37 +17,6 @@ head.onload = function() {
 
 home.onload = function() {
 	context.drawImage(home, 0, 55, 1280, 98);
-
-	// context.rect(10,55,80,22);
-	// context.stroke();
-	// context.rect(90,55,70,22);
-	// context.stroke();
-	// context.rect(160,55,145,22);
-	// context.stroke();
-	// context.rect(305,55,70,22);
-	// context.stroke();
-	// context.rect(375,55,67,22);
-	// context.stroke();
-	// context.rect(442,55,83,22);
-	// context.stroke();
-	// context.rect(525,55,72,22);
-	// context.stroke();
-
-	// context.rect(7,125,28,20);
-	// context.stroke();
-	// context.rect(35,125,28,20);
-	// context.stroke();
-
-	// context.rect(407,125,28,20);
-	// context.stroke();
-	// context.rect(351,125,28,20);
-	// context.stroke();
-
-	// context.rect(1140,95,50,55);
-	// context.stroke();
-	// context.rect(1040,95,50,55);
-	// context.stroke();
-
 };
 
 body.onload = function() {
@@ -55,7 +27,7 @@ head.src = 'screenshots/header.png';
 home.src = 'screenshots/0home.png';
 body.src = 'screenshots/body.png';
 
-// create the rectangles
+// define the active regions
 
 var home_rect = {x:10, y:55, w:80, h:22, n:0};
 var layout_rect = {x:90, y:55, w:70, h:22, n:1};
@@ -67,56 +39,81 @@ var review_rect = {x:525, y:55, w:72, h:22, n:6};
 
 var rects = [home_rect, layout_rect, docelt_rect, tables_rect, charts_rect, smartart_rect, review_rect];
 var urls = ['0home', '1layout', '2docelt', '3tables', '4charts', '5smartart', '6review'];
-// click handlers
 
-var bold = {x:7,y:125,w:28,h:20};
-var justify = {x:407,y:125,w:28,h:20};
-var insertpic = {x:1140,y:95,w:50,h:55};
-var italicize = {x:35,y:125,w:28,h:20};
-var center = {x:351,y:125,w:28,h:20};
-var inserttb = {x:1040,y:95,w:50,h:55};
+var bold = {x:7,y:125,w:28,h:20,n:0};
+var italicize = {x:35,y:125,w:28,h:20,n:1};
+var justify = {x:407,y:125,w:28,h:20,n:2};
+var center = {x:351,y:125,w:28,h:20,n:3};
+var insertpic = {x:1140,y:95,w:50,h:55,n:4};
+var inserttb = {x:1040,y:95,w:50,h:55,n:5};
 
-var orientation = {x:7,y:92,w:60,h:58};
-var size = {x:67,y:92,w:55,h:58};
+var home_rects = [bold, italicize, justify, center, insertpic, inserttb];
 
-// orientation
-// bottom margin
-// size
-// left margin
+var orientation = {x:7,y:92,w:60,h:58,n:0};
+var size = {x:67,y:92,w:55,h:58,n:1};
+var bottommar = {x:350,y:95,w:45,h:20,n:2};
+var leftmar = {x:255,y:125,w:45,h:20,n:3};
 
-// accept
-// reject
+var layout_rects = [orientation, size, bottommar, leftmar];
 
+var accept = {x:445,y:92,w:53,h:58,n:0};
+var reject = {x:497,y:92,w:50,h:58,n:1};
 
+var review_rects = [accept, reject];
+
+// check the click event
 canvas.addEventListener('click', function(e) {
-	var rect = collides(rects, e.offsetX, e.offsetY);
 
+	// check if a tab rectangle is clicked
+	var rect = collides(rects, e.offsetX, e.offsetY);
 	if (rect) {
-		// console.log(rect);
 		current_page = rect.n;
 
 		var imageObject = new Image();
 		imageObject.onload = function() {
 			context.drawImage(imageObject, 0, 55, 1280, 98);
-			context.rect(7,92,60,58);
-			context.stroke();
-			context.rect(67,92,55,58);
-			context.stroke();
 		};
 		imageObject.src = 'screenshots/'+urls[rect.n]+'.png';
 	}
 	else {
-		// alert('WRONG!');
+
+		if (current_page === 0) {
+			rect = collides(home_rects, e.offsetX, e.offsetY);
+		}
+		else if (current_page == 1) {
+			rect = collides(layout_rects, e.offsetX, e.offsetY);
+		}
+		else if (current_page == 6) {
+			rect = collides(review_rects, e.offsetX, e.offsetY);
+		}
+
+		if (rect) {
+			alert('some target clicked');
+		}
+
+		if (rect.n == target_rect) {
+			// TODO: alert correct
+			// alert('target clicked');
+		}
+		else {
+			// TODO: alert incorrect
+			// alert('nontarget clicked');
+		}
 	}
+
+	// TODO: record click timing and location
+	console.log(rect);
 });
 
-function collides(rects,x,y) {
+// see if click is on an active region
+function collides(rs,x,y) {
+
 	var isCollision = false;
-	for (var i=0, len=rects.length; i<len; i++) {
-		var left = rects[i].x, right = rects[i].x+rects[i].w;
-		var top = rects[i].y, bottom = rects[i].y+rects[i].h;
+	for (var i=0, len=rs.length; i<len; i++) {
+		var left = rs[i].x, right = rs[i].x+rs[i].w;
+		var top = rs[i].y, bottom = rs[i].y+rs[i].h;
 		if (right >= x && left <= x && bottom >= y && top <= y) {
-			isCollision = rects[i];
+			isCollision = rs[i];
 		}
 	}
 	return isCollision;
