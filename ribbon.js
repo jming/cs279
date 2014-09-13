4eecec8e0b5e76b8fe474dc31a7ca927f1c9bbea
Dianna Hu <dhu@college.harvard.edu>
2014-09-13 01:44:40 -0400

    chunk of command stuff done, including reordering and click handling. started working on display of command.

--- ribbon.js
+++ ribbon.js
@@ -1,15 +1,14 @@
 // global variables
-
 var canvas = document.getElementById('ribbon-canvas');
 var context = canvas.getContext('2d');
 var current_page = 0;
 var target_rect = 11;
 
 // initialize the basic screen
-
 var head = new Image();
 var home = new Image();
 var body = new Image();
+var command = new Image();
 
 head.onload = function() {
 	context.drawImage(head, 0, 0, 1280, 55);
@@ -23,12 +22,15 @@ body.onload = function() {
 	context.drawImage(body, 0, 155, 1280, 648);
 };
 
+command.onload = function() {
+  context.drawImage(command, 580, 300);
+}
+
 head.src = 'screenshots/header.png';
 home.src = 'screenshots/0home.png';
 body.src = 'screenshots/body.png';
 
 // define the active regions
-
 var home_rect = {x:10, y:55, w:80, h:22, n:0};
 var layout_rect = {x:90, y:55, w:70, h:22, n:1};
 var docelt_rect = {x:160, y:55, w:145, h:22, n:2};
@@ -37,35 +39,49 @@ var charts_rect = {x:375, y:55, w:67, h:22, n:4};
 var smartart_rect = {x:442, y:55, w:83, h:22, n:5};
 var review_rect = {x:525, y:55, w:72, h:22, n:6};
 
+// create rects to describe each pane
 var rects = [home_rect, layout_rect, docelt_rect, tables_rect, charts_rect, smartart_rect, review_rect];
 var urls = ['0home', '1layout', '2docelt', '3tables', '4charts', '5smartart', '6review'];
 
-var bold = {x:7,y:125,w:28,h:20,n:0};
-var italicize = {x:35,y:125,w:28,h:20,n:1};
-var justify = {x:407,y:125,w:28,h:20,n:2};
-var center = {x:351,y:125,w:28,h:20,n:3};
-var insertpic = {x:1140,y:95,w:50,h:55,n:4};
-var inserttb = {x:1040,y:95,w:50,h:55,n:5};
+// information for commands of interest
+// t (text), x and y (position), w and h (dimension), p (pane), n (number)
+var commandSets = [
+  [
+    {t:'Bold text',x:7,y:125,w:28,h:20,p:0,n:0},
+    {t:'Justify text',x:407,y:125,w:28,h:20,p:0,n:2},
+    {t:'Insert picture',x:1140,y:95,w:50,h:55,p:0,n:4},
+    {t:'Orientation',x:7,y:92,w:60,h:58,p:1,n:0},
+    {t:'Bottom margin',x:350,y:95,w:45,h:20,p:1,n:2},
+    {t:'Accept revision',x:445,y:92,w:53,h:58,p:6,n:0}
+  ],
+  [
+    {t:'Italicize text',x:35,y:125,w:28,h:20,p:0,n:1},
+    {t:'Center text',x:351,y:125,w:28,h:20,p:0,n:3},
+    {t:'Insert textbox',x:1040,y:95,w:50,h:55,p:0,n:5},
+    {t:'Size',x:67,y:92,w:55,h:58,p:1,n:1},
+    {t:'Left margin',x:255,y:125,w:45,h:20,p:1,n:3},
+    {t:'Reject revision',x:497,y:92,w:50,h:58,p:6,n:1}
+  ],
+];
 
-var home_rects = [bold, italicize, justify, center, insertpic, inserttb];
-
-var orientation = {x:7,y:92,w:60,h:58,n:0};
-var size = {x:67,y:92,w:55,h:58,n:1};
-var bottommar = {x:350,y:95,w:45,h:20,n:2};
-var leftmar = {x:255,y:125,w:45,h:20,n:3};
-
-var layout_rects = [orientation, size, bottommar, leftmar];
-
-var accept = {x:445,y:92,w:53,h:58,n:0};
-var reject = {x:497,y:92,w:50,h:58,n:1};
-
-var review_rects = [accept, reject];
+// info about user
+var studyInfo = {
+  // index of command set used
+  setId: 0,
+  // index of block used (0: familiarization, 1: performance)
+  blockId: 0,
+  // index of trial within block (30 total for familiarization, 90 total for performance)
+  trialId: 0,
+  // data that holds participant accuracy and timing
+  data: []
+};
 
 // check the click event
 canvas.addEventListener('click', function(e) {
-
-	// check if a tab rectangle is clicked
-	var rect = collides(rects, e.offsetX, e.offsetY);
+  var command = commandSets[studyInfo.setId][studyInfo.trialId % commandSets[0].length];
+  
+	// re-render if any pane is clicked
+	 var rect = collides(rects, e.offsetX, e.offsetY);
 	if (rect) {
 		current_page = rect.n;
 
@@ -75,39 +91,27 @@ canvas.addEventListener('click', function(e) {
 		};
 		imageObject.src = 'screenshots/'+urls[rect.n]+'.png';
 	}
+	
+	// check if correct pane clicked
+	if (collides([rects[command.p]], e.offsetX, e.offsetY)) {
+	  // TODO: add to data here?
+	}
+	// check if correct command clicked
+	else if (collides([command], e.offsetX, e.offsetY)) {
+		// alert('some command clicked');
+		studyInfo.trialId++;
+	}
+	// neither correct pane nor command clicked
 	else {
-
-		if (current_page === 0) {
-			rect = collides(home_rects, e.offsetX, e.offsetY);
-		}
-		else if (current_page == 1) {
-			rect = collides(layout_rects, e.offsetX, e.offsetY);
-		}
-		else if (current_page == 6) {
-			rect = collides(review_rects, e.offsetX, e.offsetY);
-		}
-
-		if (rect) {
-			alert('some target clicked');
-		}
-
-		if (rect.n == target_rect) {
-			// TODO: alert correct
-			// alert('target clicked');
-		}
-		else {
-			// TODO: alert incorrect
-			// alert('nontarget clicked');
-		}
+	 // alert('boooooo');
 	}
 
 	// TODO: record click timing and location
-	console.log(rect);
+	console.log(e.offsetX + '\n' + e.offsetY);
 });
 
-// see if click is on an active region
+// see if click is on one of active regions in rs
 function collides(rs,x,y) {
-
 	var isCollision = false;
 	for (var i=0, len=rs.length; i<len; i++) {
 		var left = rs[i].x, right = rs[i].x+rs[i].w;
@@ -119,3 +123,44 @@ function collides(rs,x,y) {
 	return isCollision;
 }
 
+// shuffles commands in user's command set
+function shuffleCommandSet() {
+  var commandSet = commandSets[studyInfo.setId];
+  for (var i = commandSet.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = commandSet[i];
+      commandSet[i] = commandSet[j];
+      commandSet[j] = temp;
+  }
+}
+
+// generate new order of commands safely, i.e., such that at least half involve tab switch
+function safelyReorderCommands() {
+  shuffleCommandSet();
+  var numSwitches = 0;
+  var commandSet = commandSets[studyInfo.setId];
+  for (var i = 1; i < commandSet.length; i++) {
+    // yay, we need a tab switch
+    if (commandSet[i].p != commandSet[i - 1].p) {
+      numSwitches++;
+    }
+  }
+  
+  if (numSwitches < 0.5 * commandSet.length) {
+    safelyReorderCommands();
+  }
+}
+
+// wrapper for drawing target command onto document (literally)
+function drawCommand(pane, number) {
+	command.src = 'screenshots/icons/' + pane + number + '.png';
+}
+
+function init() {
+  safelyReorderCommands();
+  var firstCommand = commandSets[studyInfo.setId][0];
+  // drawCommand(firstCommand.p, firstCommand.n);
+  drawCommand(0, 5);
+}
+
+window.onload = init;
