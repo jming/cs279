b2b9c7c776dd4802c8ed39a109c1227d53f0492f
Dianna Hu <dhu@college.harvard.edu>
2014-10-31 19:09:57 -0400

    map global

--- finalproject/app.js
+++ finalproject/app.js
@@ -1,9 +1,12 @@
+// yay globals :)
+var map;
+
 function initialize() {
   var mapOptions = {
     center: { lat: 42.370788, lng: -71.117111},
     zoom: 15
   };
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
+  map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
