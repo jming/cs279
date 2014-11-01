26d32feb224eb0058a701c78fdd46c319760b643
jming <jming@college.harvard.edu>
2014-10-31 20:12:30 -0400

    colored markers

--- finalproject/app.js
+++ finalproject/app.js
@@ -1,6 +1,12 @@
 // yay globals :)
 var map;
 
+function MarkerInfo (lat,lng,highlight) {
+  this.lat = lat;
+  this.lng = lng;
+  this.highlight = highlight;
+} 
+
 function initialize() {
   var mapOptions = {
     center: { lat: 42.370788, lng: -71.117111},
@@ -8,16 +14,10 @@ function initialize() {
   };
   window.map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
-  placeMarkers(map,[[42.365517, -71.122176], [42.366523, -71.119212]]);
+  placeMarkers(map,[
+    new MarkerInfo(42.365517, -71.122176, true),
+    new MarkerInfo(42.366523, -71.119212, false)]);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
-
-var marker_pos = new google.maps.LatLng(42.364270, -71.124148)
-
-var marker = new google.maps.Marker({
-  position: marker_pos,
-  map: map,
-  title: 'Hello'
-});
 // console.log(window.map)
--- finalproject/marker.js
+++ finalproject/marker.js
@@ -5,14 +5,30 @@ function placeMarkers(map, markers_list) {
 	for (var i=0; i < markers_list.length; i++) {
 		// console.log(markers_list[i]);
 		var marker_pos = new google.maps.LatLng(
-			markers_list[i][0], markers_list[i][1]);
+			markers_list[i].lat, markers_list[i].lng);
+
+		var icon_color = (markers_list[i].highlight) ? '' : 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
 
 		var marker = new google.maps.Marker({
 			position: marker_pos,
 			map: map,
-			title: 'Hello'
+			title: 'Intersection ' + i.toString(),
+			clickable: true,
+			icon: icon_color
+
+			// http://maps.google.com/mapfiles/ms/icons/blue-dot.png
+			// http://maps.google.com/mapfiles/ms/icons/red-dot.png
+			// http://maps.google.com/mapfiles/ms/icons/purple-dot.png
+			// http://maps.google.com/mapfiles/ms/icons/yellow-dot.png
+			// http://maps.google.com/mapfiles/ms/icons/green-dot.png
+		});
+
+		google.maps.event.addListener(marker, 'click', function(event) {
+			console.log(event);
 		});
 	}
+
+
 
 
 }
