956024a6fd51093f6259d16d19c35e0f2f3396dc
jming <jming@college.harvard.edu>
2014-11-02 16:27:20 -0500

    step2 setup done, instructions needs to be more clear

--- finalproject/step2/app.js
+++ finalproject/step2/app.js
@@ -6,30 +6,35 @@ var globals = {
   markersArray: []
 };
 
-function MarkerInfo (lat,lng,highlight) {
+function MarkerInfo (lat,lng,highlight,highlight_start) {
   this.lat = lat;
   this.lng = lng;
   this.highlight = highlight;
+  this.highlight_start = highlight_start;
 }
 
 function initialize() {
-  globals.directionsDisplay = new google.maps.DirectionsRenderer();
-  globals.directionsService = new google.maps.DirectionsService();
+  // globals.directionsDisplay = new google.maps.DirectionsRenderer();
+  // globals.directionsService = new google.maps.DirectionsService();
+
+  var center_pos = new google.maps.LatLng(42.370788, -71.117111);
 
   var mapOptions = {
-    center: { lat: 42.370788, lng: -71.117111},
+    center: center_pos,
     zoom: 15
   };
 
   globals.map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
-  globals.directionsDisplay.setMap(globals.map);
+  // globals.directionsDisplay.setMap(globals.map);
 
   placeMarkers(globals.map, [
-    new MarkerInfo(42.365517, -71.122176, true),
-    new MarkerInfo(42.366523, -71.119212, false)
+    new MarkerInfo(42.365517, -71.122176, true, true),
+    new MarkerInfo(42.366523, -71.119212, false, false)
   ]);
+
+
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
