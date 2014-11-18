dd3ed3b23ddf81a7cf5c89e6b9bccaf5e3903900
Dianna Hu <dhu@college.harvard.edu>
2014-11-18 09:40:55 -0500

    mid obstacles done

--- finalproject/step7/sections.js
+++ finalproject/step7/sections.js
@@ -1,15 +1,23 @@
 function showSectionMarkers(sections) {
     for (var i = 0; i < sections.length; i++) {
         var section = sections[i];
+        var section_img = '';
 
         if (section.type == 'mid') {
-            // skip if no obstacles
+            // show all obstacles with their position
             for (var oId = 0; oId < section.obstacles.length; oId++) {
-                console.log('TODO');                
+                var obstacle = section.obstacles[oId];
+                section_img = '../img/obs-' + obstacle.side + '-resize.png';
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(obstacle.loc.k, obstacle.loc.B),
+                    map: globals.map,
+                    title: 'Obstacle ' + oId.toString(),
+                    clickable: true,
+                    icon: section_img
+                });
             }
         }
         else {
-            var section_img = '';
             if (section.type == 'lrud') {
                 section_img = '../img/int-four-4.png';
             }
@@ -17,10 +25,8 @@ function showSectionMarkers(sections) {
                 section_img = '../img/int-three-3.png';
                 // section_img = '../img/int-four-4.png';
             }
-            var lat = section.loc.k;
-            var lng = section.loc.B;
             var marker = new google.maps.Marker({
-                position: new google.maps.LatLng(lat, lng),
+                position: new google.maps.LatLng(section.loc.k, section.loc.B),
                 map: globals.map,
                 title: 'Intersection ' + i.toString(),
                 clickable: true,
