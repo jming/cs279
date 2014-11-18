27e8282349f6a620bba219b9dc39fcd6207c0693
Dianna Hu <dhu@college.harvard.edu>
2014-11-17 22:27:11 -0500

    add step7 with alternate routing system

---
+++ finalproject/step7/sections.js
@@ -0,0 +1,9 @@
+globals.green = ['#0F9D58', '#7AEAC5'];
+globals.red = ['#DB4437', '#F5A2BD'];
+globals.weight = 7;
+
+// display sections of routes color-coded by accessibility
+// section - Section object (see app.js)
+function showRouteAccessibility(section) {
+    getRoute(section);
+}
